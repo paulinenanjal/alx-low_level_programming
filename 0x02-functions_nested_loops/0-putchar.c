e6b7cf7f69cf3bf436ed54de182b9b09485c8acf
paulinenanjal <paulinenanjal>
2022-07-06 05:54:32 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,14 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Success)
- **/
+ *     */
 int main(void)
 {
-const char str[] = "_putchar\n";
-int len = strlen(str);
-int i;
+		const char str[] = "_putchar\n";
+			int len = strlen(str);
+				int i;
 
-for (i = 0; i < len; i++)
-_putchar(str[i]);
-return (0);
+					for (i = 0; i < len; i++)
+								_putchar(str[i]);
+						return (0);
 }
