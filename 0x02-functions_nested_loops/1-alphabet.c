bda651c99547ca2beeee45b521d9348192955ca6
paulinenanjal <paulinenanjal>
2022-07-06 06:51:11 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,9 +7,9 @@
 
 void print_alphabet(void)
 {
-	char alphabet;
+char alphabet;
 
-	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		_putchar(alphabet);
-	_putchar('\n');
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+_putchar(alphabet);
+_putchar('\n');
 }
