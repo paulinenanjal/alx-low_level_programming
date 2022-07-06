cda57605ab6c8a1faece2ae903da89ba5d34fbac
paulinenanjal <paulinenanjal>
2022-07-06 06:53:43 -0700

    print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,13 +7,13 @@
 
 void print_alphabet_x10(void)
 {
-	char alphabet;
-	int i;
+char alphabet;
+int i;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-			_putchar(alphabet);
-		_putchar('\n');
-	}
+for (i = 0; i < 10; i++)
+{
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+_putchar(alphabet);
+_putchar('\n');
+}
 }
