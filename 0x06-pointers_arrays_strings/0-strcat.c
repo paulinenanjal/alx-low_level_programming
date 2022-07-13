1bef6e91bf0cf375f0312672eb0c4cea0271f08d
paulinenanjal <paulinenanjal>
2022-07-13 00:36:14 -0700

    All mandatory tasks

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
-/**
- *  * _strcat - concatenate two strings
- *   *
- *    * Return: char *
- **/
 
+/**
+ * * _strcat -> this is a funcion strcat
+ * * @dest: first param
+ * * @src: second param
+ * * Return: a string
+ **/
 char *_strcat(char *dest, char *src)
 {
-int len, _len, i;
-i = 0;
-len = strlen(src);
-_len = strlen(dest);
-						
-for (i = 0; i < len; i++)
+int len = 0, i;
+
+while (dest[len])
+len++;
+for (i = 0; src[i] != 0; i++)
 {
-dest[_len + i] = src[i];
+dest[len] = src[i];
+len += 1;			
 }
+dest[len] = '\0';
 return (dest);
 }
