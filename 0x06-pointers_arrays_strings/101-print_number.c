9833a7a5fdb69f04a60a399718ac6c06bad411cc
paulinenanjal <paulinenanjal>
2022-07-14 11:21:05 -0700

    Task 8

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,33 +1,50 @@
-#include"main.h"
+#include "main.h"
 
 /**
-*  * print_number - print an integer, without using long, arrays, or pointers
-*   * @n: number to be printed
-**/
+ *  * print_number - print an integer, without using long, arrays, or pointers
+ *   * @n: number to be printed
+ **/
 
-voidprint_number(int n)
+void print_number(int n)
 {
-unsignedint tens, dig, pos = n;
-doubletemp_tens = 1;
-if(n == 0)
+unsigned int tens, dig, pos = n;
+double temp_tens = 1;
+
+if (n == 0)
 _putchar('0');
 else
 {
-if(n < 0)
+if (n < 0)
+
 {
-pos= n * -1;
+
+pos = n * -1;
+
 _putchar('-');
+		
 }
 
-while(temp_tens <= pos)
-temp_tens*= 10;	
-tens= temp_tens / 10;
-while(tens >= 1)	
-{
-dig= pos / tens;
-_putchar(dig+ '0');
-pos= (pos - (tens *dig));
-tens/= 10;
 
+while (temp_tens <= pos)
+	
+temp_tens *= 10;
+	
+tens = temp_tens / 10;
+
+
+while (tens >= 1)
+	
+{
+
+dig = pos / tens;
+
+_putchar(dig + '0');
+	
+pos = (pos - (tens * dig));
+
+tens /= 10;
+
+}
+		
 }
 }
