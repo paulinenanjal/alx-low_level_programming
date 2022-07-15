e06c5bc1cc4d93fed6c2ebaf0146e3898864f536
paulinenanjal <paulinenanjal>
2022-07-15 11:35:37 -0700

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -20,31 +20,29 @@ if (n < 0)
 
 pos = n * -1;
 
-_putchar('-');
-		
+_putchar('-);
 }
 
 
 while (temp_tens <= pos)
-	
+
 temp_tens *= 10;
-	
+
 tens = temp_tens / 10;
 
 
 while (tens >= 1)
-	
-{
 
+{
 dig = pos / tens;
 
 _putchar(dig + '0');
-	
+
 pos = (pos - (tens * dig));
 
 tens /= 10;
 
 }
-		
+
 }
 }
