c415bf6662d1c91a66473fd0fd4890bb06e88df2
paulinenanjal <paulinenanjal>
2022-07-26 00:16:45 -0700

    Task 3 betty correction

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -5,7 +5,7 @@
  * * @size: size of array to be created
  * * @c: character an array is initialized with
  * * Return: a pointer to an array
- **/
+ */
 char *create_array(unsigned int size, char c)
 {
 unsigned int i;
